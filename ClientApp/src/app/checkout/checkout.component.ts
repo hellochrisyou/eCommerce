cab975252249347da9032d8332c79ab2b5323b1d
Chris You <chrisyoufacebook@gmail.com>
2018-11-15 14:56:42 -0500

    Fixed Front end Layout (css)

--- ClientApp/src/app/checkout/checkout.component.ts
+++ ClientApp/src/app/checkout/checkout.component.ts
@@ -3,8 +3,9 @@ import { ShoppingcartService } from '../Services/shoppingcart.service';
 import { MatSnackBar } from '@angular/material';
 import { MakeService } from '../Services/make.service';
 import { AuthService } from '../Services/auth.service';
-import { ThisOrder } from '../Models/interfaces';
+import { ThisOrder, OrderWDate } from '../Models/interfaces';
 import { Router } from '@angular/router';
+import 'rxjs/add/operator/finally';
 
 declare let paypal: any;
 
@@ -13,63 +14,15 @@ declare let paypal: any;
   templateUrl: './checkout.component.html',
   styleUrls: ['./checkout.component.css']
 })
-export class CheckoutComponent implements OnInit, AfterViewChecked {
-    // Paypal
-    paypalConfig = {
-        env: 'sandbox',
-        client: {
-            sandbox: 'Af29uGTg9cTe0QxVYNu8lx89ecHuoo1uuX0fGorDfhYXJ57Uj9sHVqiYm2_IvnYf_ER39Xc-Lwbe6miY',
-            production: '<your-production-key-here>'
-        },
-        commit: true,
-        payment: (data, actions) => {
-            return actions.payment.create({
-                payment: {
-                    transactions: [{
-                        amount: {
-                            total: this.total_price,
-                            currency: 'USD'
-                        }
-                    }]
-                }
-            });
-        },
-        onAuthorize: (data, actions) => {
-            return actions.payment.execute().then((payment) => {
-                // Do something when payment is successful.
-                for (const tmp in this.shoppingOrder) {
-                    if (this.shoppingOrder.hasOwnProperty(tmp)) {
-                        this.tmpObject.address = this.shoppingOrder.address;
-                        this.tmpObject.state = this.stateValue;
-                        this.tmpObject.zipCode = this.shoppingOrder.zipCodeValue;
-                        this.tmpObject.order_Number = Math.floor((Math.random() * 999999) + 1000000).toString();
-                        this.tmpObject.accountInfoOrderId = localStorage.getItem('account_id');
-                        this.tmpObject.case = this.shoppingOrder[tmp].case;
-                        this.tmpObject.cooling_Fan = this.shoppingOrder[tmp].cooling_Fan;
-                        this.tmpObject.cpu = this.shoppingOrder[tmp].cpu;
-                        this.tmpObject.gpu = this.shoppingOrder[tmp].gpu;
-                        this.tmpObject.motherboard = this.shoppingOrder[tmp].motherboard;
-                        this.tmpObject.power_Supply = this.shoppingOrder[tmp].power_Supply;
-                        this.tmpObject.ram = this.shoppingOrder[tmp].ram;
-                        this.tmpObject.storage = this.shoppingOrder[tmp].storage;
-                        this.tmpObject.total_Price = this.shoppingOrder[tmp].total_Price;
-                        this.tmpObject.type = this.shoppingOrder[tmp].type;
-                        this.makeService.CreateOrder(this.tmpObject).subscribe(x => x);
-                        this.OpenSnackbar();
-                    }
-                }
-            });
-        }
-    };
-
+export class CheckoutComponent implements OnInit {
   // Variables
-  addScript = false;
-  paypalLoad = true;
   total_price = 0;
   finalAmount = 1;
   addressValue = '';
   stateValue = '';
   zipCodeValue = '';
+  lastOrderNumber: string[];
+  order_Number = '';
 
   // Objects
   shoppingOrder: any = {};
@@ -90,6 +43,7 @@ export class CheckoutComponent implements OnInit, AfterViewChecked {
       storage: '',
       total_Price: ''
   };
+  order: any;
 
   constructor(
       private router: Router,
@@ -106,30 +60,23 @@ export class CheckoutComponent implements OnInit, AfterViewChecked {
       this.shoppingOrder = this.ShoppingcartServices.Get();
       for (const tmp in this.shoppingOrder) {
          if (this.shoppingOrder.hasOwnProperty(tmp)) {
-           this.total_price = this.total_price + parseInt(this.shoppingOrder[tmp].total_Price, 10);
-           console.log('totalp', this.total_price);
-         }
+           this.total_price = this.total_price + parseInt(this.shoppingOrder[tmp].total_Price, 10);         }
       }
    }
 
-
-  ngAfterViewChecked() {
-      if (!this.addScript) {
-          this.AddPaypalScript().then(() => {
-              paypal.Button.render(this.paypalConfig, '#paypal-checkout-btn');
-              this.paypalLoad = false;
-          });
-      }
-  }
-  AddPaypalScript() {
-      this.addScript = true;
-      return new Promise((resolve, reject) => {
-          const scripttagElement = document.createElement('script');
-          scripttagElement.src = 'https://www.paypalobjects.com/api/checkout.js';
-          scripttagElement.onload = resolve;
-          document.body.appendChild(scripttagElement);
-      });
-  }
+   submit() {
+        this.tmpObject = this.shoppingOrder;
+        this.tmpObject.address = this.addressValue;
+        this.tmpObject.state = this.stateValue;
+        this.tmpObject.zipCode = this.zipCodeValue;
+        this.makeService.GetOrder().subscribe( x => {
+            console.log('this.tmpObject', x[x.length - 1]);
+            this.tmpObject.order_Number = (Math.floor(Math.random() * 9999999) +  x[x.length - 1].order_Number.toString());
+            console.log('this.tmpObject', this.tmpObject.order_Number);
+            this.makeService.CreateOrder(this.tmpObject).subscribe();
+            console.log(this.lastOrderNumber);
+        });
+    }
 
   OpenSnackbar() {
       this.snackBar.openFromComponent(OrderCompleteSnackComponent, {
